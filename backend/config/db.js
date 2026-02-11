af8d7b7e48fb18d39f10ae1c7628ef2e982ec884
kumar13roshan <kumarroshan39620@gmail.com>
2026-02-11 22:13:40 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,14 +1,11 @@
 import mongoose from "mongoose";
 
 const connectDB = async () => {
-  const uri =
-    process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/primepicks";
-
   try {
-    await mongoose.connect(uri);
+    await mongoose.connect(process.env.MONGO_URI);
     console.log("MongoDB Connected");
-  } catch (err) {
-    console.error("MongoDB connection error:", err);
+  } catch (error) {
+    console.error("MongoDB connection error:", error);
   }
 };
 
