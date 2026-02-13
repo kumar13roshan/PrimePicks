9cc614619c3fc280277daff89cff323c7e3326ca
kumar13roshan <kumarroshan39620@gmail.com>
2026-02-13 11:43:49 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -23,8 +23,9 @@ connectDB();
 app.get("/api/health", (req, res) => {
   res.json({ status: "ok" });
 });
-app.use("/api", adminRoutes);
 app.use("/api", requireAuth);
+
+app.use("/api", adminRoutes);
 app.use("/api", purchaseRoutes);
 app.use("/api", saleRoutes);
 app.use("/api", stockRoutes);
